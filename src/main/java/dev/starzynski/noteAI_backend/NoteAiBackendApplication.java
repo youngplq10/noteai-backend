8d65851088cb2c919c9ca902b02bc8dc9639f412
youngplq10 <patryk.plq@o2.pl>
2025-02-20 16:42:48 +0100

    Initial commit

---
+++ src/main/java/dev/starzynski/noteAI_backend/NoteAiBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.starzynski.noteAI_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/starzynski/noteAI_backend/NoteAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starzynski.noteAI_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
